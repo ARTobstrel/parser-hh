82aa35416b84e199f591670d3cdda1e4694a6cd9
Artem Fadeev <artemfap@yandex.ru>
2020-01-07 02:09:03 +0300

    commit3csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup as bs
 
 
@@ -8,6 +9,8 @@ headers = {'accept': '*/*',
 base_url = 'https://hh.ru/search/vacancy?search_period=3&area=1&text=python&page=0'
 
 jobs = []
+urls = []
+urls.append(base_url)
 
 def hh_parse(base_url, headers):
     session = requests.Session() #создаем одну сессию. Создаем эмуляцию того что мы ищем вакансию
@@ -15,23 +18,46 @@ def hh_parse(base_url, headers):
 
     if request.status_code == 200:
         soup = bs(request.content, 'lxml')
-        divs = soup.find_all('div', class_='vacancy-serp-item')
-        for div in divs:
-            title = div.find('a', class_='bloko-link').text
-            href = div.find('a', class_='bloko-link')['href']
-            company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
-            text1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
-            text2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
-            content = f'{text1} {text2}'
-            jobs.append({
-                'title': title,
-                'href': href,
-                'company': company,
-                'content': content
-            })
+        try:
+            pagination = soup.find_all('a', attrs={'data-qa': 'pager-page'})
+            count = int(pagination[-1].text)
+            for i in range(count):
+                url = f'https://hh.ru/search/vacancy?search_period=3&area=1&text=python&page={i}'
+                if url not in urls:
+                    urls.append(url)
+        except:
+            pass
+        for url in urls:
+            request = session.get(url, headers=headers)
+            soup = bs(request.content, 'lxml')
+            divs = soup.find_all('div', class_='vacancy-serp-item')
+
+            for div in divs:
+                title = div.find('a', class_='bloko-link').text
+                href = div.find('a', class_='bloko-link')['href']
+                company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+                text1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
+                text2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
+                content = f'{text1} {text2}'
+                jobs.append({
+                    'title': title,
+                    'href': href,
+                    'company': company,
+                    'content': content
+                })
+            print(len(jobs))
     else:
         print('ERROR')
 
+    return jobs
+
+def files_writer(jobs):
+    with open('parsed_jobs.csv', 'w') as file:
+        a_pen = csv.writer(file)
+        a_pen.writerow({'Вакансия', 'URL', 'Компания', 'Описание'})
+        for job in jobs:
+            a_pen.writerow((job['title'], job['href'], job['company'], job['content']))
+
 if __name__ == '__main__':
-    hh_parse(base_url, headers)
-    print(jobs)
+    jobs = hh_parse(base_url, headers)
+    files_writer(jobs)
